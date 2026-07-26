ba135346638ae03103990d3aeb8f7ee108bfa2e4
refaisuhaan <suhaanrefai@gmail.com>
2026-07-26 06:10:58 +0530

    app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from prometheus_flask_exporter import PrometheusMetrics
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)   # <-- enables /metrics
+
+@app.route("/")
+def hello():
+    return "Hello World!"                   
+
+@app.route("/new")
+def new():
+    return "FINAL CHECK"            
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
